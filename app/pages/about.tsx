782d69242b4810a477eeb56a442f4518cb42d136
Flojito <flojito@archireport.com>
2024-11-21 22:49:45 +0100

    refactor: color styles (text + bg)

--- app/pages/about.tsx
+++ app/pages/about.tsx
@@ -2,6 +2,8 @@ import { IconMail, IconMapPin, IconPhone } from '@tabler/icons-react';
 import { Image } from 'antd';
 import Link from 'next/link';
 import { companyInfo } from '../constants';
+import { bgColor, textColor } from '../page';
+import { twMerge } from 'tailwind-merge';
 
 const t = {
   fr: {
@@ -68,7 +70,7 @@ export default function About() {
         <div className="container mx-auto px-4">
           <h2 className="text-3xl font-bold text-center mb-8">{t['fr'].OurLocation}</h2>
           <div className="flex flex-col md:flex-row items-center justify-center gap-8">
-            <div className="w-full md:w-1/2 h-64 bg-gray-300 rounded-lg overflow-hidden">
+            <div className="w-full md:w-1/2 h-80 bg-gray-300 rounded-lg overflow-hidden">
               <iframe
                 src="https://www.openstreetmap.org/export/embed.html?bbox=-4.312176704406739%2C48.18806693411636%2C-4.2569875717163095%2C48.2107219419585&amp;layer=mapnik&amp;marker=48.19939569036789%2C-4.284582138061523"
                 style={{ border: 0, width: '100%', height: '100%' }}
@@ -76,15 +78,15 @@ export default function About() {
             </div>
             <div className="w-full md:w-1/2 text-center">
               <h3 className="text-xl font-semibold mb-2">{companyInfo.companyName}</h3>
-              <div className="text-gray-900 dark:text-gray-400 mb-2 flex items-center justify-center">
+              <div className={twMerge(textColor, 'mb-2 flex items-center justify-center')}>
                 <IconMapPin className="mr-2" size={18} />
                 {companyInfo.address}
               </div>
-              <div className="text-gray-900 dark:text-gray-400 mb-2 flex items-center justify-center">
+              <div className={twMerge(textColor, 'mb-2 flex items-center justify-center')}>
                 <IconPhone className="mr-2" size={18} />
                 <Link href={`tel:${companyInfo.phone.replaceAll(' ', '')}`}>{companyInfo.phone}</Link>
               </div>
-              <div className="text-gray-900 dark:text-gray-400 mb-2 flex items-center justify-center">
+              <div className={twMerge(textColor, 'mb-2 flex items-center justify-center')}>
                 <IconMail className="mr-2" size={18} />
                 <Link href={`mailto:${companyInfo.email}`}>{companyInfo.email}</Link>
               </div>
@@ -92,7 +94,7 @@ export default function About() {
               <h3 className="text-xl font-semibold mb-2">{t['fr'].WorkingArea}</h3>
               <ul className="ml-4 mb-2 flex flex-wrap">
                 {workingCities.map((city, index) => (
-                  <li key={index} className="text-gray-900 dark:text-gray-400 mb-2 w-1/2">
+                  <li key={index} className={twMerge(textColor, 'mb-2 w-1/2')}>
                     {city}
                   </li>
                 ))}
@@ -102,14 +104,14 @@ export default function About() {
         </div>
       </section>
 
-      <section className="bg-green-50 dark:bg-gray-900 py-12">
+      <section className={twMerge(bgColor, 'py-12')}>
         <div className="container mx-auto px-4">
           <h2 className="text-3xl font-bold text-center mb-8">{t['fr'].EcoFriendlyCommitment}</h2>
           <div className="flex flex-col md:flex-row items-center justify-center gap-8">
             <Image width={150} height={150} src="/EcoLabel.png" alt={t['fr'].EcoFriendlyLabel} className="w-32 h-32" />
             <div className="max-w-2xl">
-              <p className="text-gray-900 dark:text-gray-400 mb-4">{t['fr'].EcoFriendlyCommitmentDescription}</p>
-              <p className="text-gray-900 dark:text-gray-400">{t['fr'].EcoFriendlyCommitmentDescription2}</p>
+              <p className={twMerge(textColor, 'mb-4')}>{t['fr'].EcoFriendlyCommitmentDescription}</p>
+              <p className={textColor}>{t['fr'].EcoFriendlyCommitmentDescription2}</p>
             </div>
           </div>
         </div>
@@ -121,7 +123,7 @@ export default function About() {
           <div className="grid grid-cols-1 md:grid-cols-2 gap-8 items-center">
             <div>
               <h2 className="text-2xl font-semibold text-center mb-2">{companyInfo.founder}</h2>
-              <p className="text-center text-gray-900 dark:text-gray-400 mb-4">{t['fr'].Founder}</p>
+              <p className={twMerge(textColor, 'mb-4 text-center')}>{t['fr'].Founder}</p>
               <Image
                 src="/placeholder.svg?height=400&width=400&text=Manager+Photo"
                 alt="Manager"
@@ -130,8 +132,8 @@ export default function About() {
             </div>
             <div>
               <h3 className="text-xl font-semibold mb-4">{t['fr'].OurStory}</h3>
-              <p className="text-gray-900 dark:text-gray-400 mb-4">{t['fr'].OurStoryDescription}</p>
-              <p className="text-gray-900 dark:text-gray-400">{t['fr'].OurStoryDescription2}</p>
+              <p className={twMerge(textColor, 'mb-4')}>{t['fr'].OurStoryDescription}</p>
+              <p className={textColor}>{t['fr'].OurStoryDescription2}</p>
             </div>
           </div>
         </div>
