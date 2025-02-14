acdc03552246f26bd6cc6a49266dbfe83131c0b3
Flojito <flojito@archireport.com>
2025-02-14 20:21:57 +0100

    build: add missing tailwind breakpoints

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,17 @@ const config: Config = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    transparent: 'transparent',
+    current: 'currentColor',
+    screens: {
+      '2xs': '320px',
+      xs: '480px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
+    },
     transitionProperty: {
       DEFAULT: 'all',
     },
